972cedbcc90618277adab44791e4e2f6e5e1536f
eodemao-cat <1399646240@qq.com>
2019-11-15 15:00:19 +0800

    webpack

---
+++ app/app.js
@@ -0,0 +1,18 @@
+//加载app.less
+require('../less/app.less');
+
+window.onload = function() {
+
+    let person = ['kevin', 'mark', 'iven'];
+
+    let ul = document.createElement('ul');
+
+    person.map(v => {
+        let li = document.createElement('li');
+        li.textContent = v;
+        ul.appendChild(li);
+    })
+
+    document.body.appendChild(ul);
+
+}
